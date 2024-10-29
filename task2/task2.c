c13af648c34419d90a1f7700a365a33c0cf644a2
binahmed-elead <abdullahelead@gmail.com>
2024-10-29 15:41:49 +0300

    fixed missing part in task2 and added the document

--- task2/task2.c
+++ task2/task2.c
@@ -8,6 +8,13 @@ int main()
 	printf("a : 0x%x\n",a);
 	printf("%p: 0x%x\n",ptr,*ptr);
 	printf("%p: 0x%x\n",ptr+1,*(ptr+1));
+	if(sizeof(ptr) == 8)
+	{
+		printf("System is 64-bit\n");
+	}else
+	{
+		printf("System is 32-bit\n");
+	}
 	if(*ptr == 0x20)
 	{
 		printf("System is little Endian\n");
